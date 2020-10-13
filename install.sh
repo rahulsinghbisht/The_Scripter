5a333af6ad45a2a09e8c5799dbf7ce000cf55b0a
manitorpotterk <45949793+manitorpotterk@users.noreply.github.com>
2020-10-13 03:04:13 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
-#!/bin/bash
+#!/bin/bash                                                                                                        
+                                                                                                                   
+echo('Installing Stabilize Shell Dependencies')                                                                    
+                                                                                                                   
+apt-get install xdotool                                                                                            
+sudo apt-get install xautomation                                                                                   
+                                                                                                                   
+echo('Installing SSH Cracker Dependencies')                                                                        
+                                                                                                                   
+apt-get install john -y                                                                                            
+                                                                                                                   
+echo('Installing Hash Cracker Dependencies')                                                                       
 
-echo('Installing Stabilize Shell Dependencies')
-
-apt-get install xdotool
-sudo apt-get install xautomation
-
-echo('Installing SSH Cracker Dependencies')
-
-apt-get install john -y
-
+pip3 install futures
+pip3 install argparse
+pip3 install requests
 
